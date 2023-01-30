58196de739a75177b9393994abb19fbbf1d3008b
Danny Godwin <63467988+dannypgodwin@users.noreply.github.com>
2023-01-30 00:47:28 -0500

    Commiting assignment + lab.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+  return ()
 
-  );
+  ;
 }
 
 export default App;
