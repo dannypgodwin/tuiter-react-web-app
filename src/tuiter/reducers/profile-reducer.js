ce01502dd3ada82759880aa5b2dbcfc7a7a060b1
Danny Godwin <63467988+dannypgodwin@users.noreply.github.com>
2023-03-26 20:47:31 -0400

    Committing A7.

---
+++ src/tuiter/reducers/profile-reducer.js
@@ -0,0 +1,38 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    profile: {
+        "firstName": "Danny",
+        "lastName": "Godwin",
+        "handle": "@dannygodwin",
+        "profilePicture": "../../imgs/profile_pic.jpg",
+        "profile_width": 70,
+        "profile_height": 100,
+        "bannerPicture": "../../imgs/banner.jpg",
+        "banner_width": "100%",
+        "banner_height": 200,
+        "bio": "Data engineer and musician",
+        "website": "gooogle.com",
+        "location": "Boston, MA",
+        "dateOfBirth": "Dec 8, 1993",
+        "dateJoined": "September, 2013",
+        "tweets": "0",
+        "followingCount": 0,
+        "followersCount": 100
+    }
+}
+
+const profileSlice = createSlice(
+    {
+        name: 'profile',
+        initialState: initialState,
+        reducers:{
+            updateProfile: (state, action) => {
+                state.profile = action.payload;
+            },
+        }
+    }
+);
+
+export const { updateProfile } = profileSlice.actions;
+export default profileSlice.reducer;
