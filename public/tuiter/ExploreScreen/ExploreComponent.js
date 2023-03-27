7ca14ac1b228a262634daf9d7ef0e4b0cb13a730
Danny Godwin <63467988+dannypgodwin@users.noreply.github.com>
2023-03-26 21:05:27 -0400

    Committing A7.

--- public/tuiter/ExploreScreen/ExploreComponent.js
+++ public/tuiter/ExploreScreen/ExploreComponent.js
@@ -1,35 +1,44 @@
-import PostSummaryList from '../PostSummaryList/index.js';
+import React from "react";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import PostSummaryList from "../post-summary-list/index.js";
 
-const ExploreComponent = () => {
-    const trendingTopics = ['For you', 'Trending', 'News', 'Sports', 'Entertainment'];
+const tabs = ["For you", "Trending", "News", "Sports", "Entertainment"];
 
-    const navTabs = trendingTopics.map((topic, index) => {
-        const isActive = index === 0 ? 'active' : '';
+function ExploreComponent() {
+    const renderTabs = () => {
+        return tabs.map((title, index) => {
+            const className = index === 0 ? "nav-link active" : "nav-link";
+            return (
+                <li key={index} className="nav-item">
+                    <a className={className} href="#top">{title}</a>
+                </li>
+            );
+        });
+    }
 
-        return `
-            <li class="nav-item">
-                <a class="nav-link ${isActive}" aria-current="page" href="#">${topic}</a>
-            </li>
-        `;
-    }).join('');
-
-    return `
-        <div class="wd-searchbar mb-2">
-            <i class="fa-solid fa-magnifying-glass wd-search-icon text-secondary ms-2"></i>
-            <input type="text" placeholder="Search Twitter" class="form-control rounded-5 ps-5">
-            <i class="fa-solid fa-gear fa-xl mx-3 text-primary"></i>
-        </div>
-        <ul class="nav nav-tabs mb-2 d-flex justify-content-between">
-            ${navTabs}
-        </ul>
-        <div class="position-relative mb-2">
-            <img src="../../assets/starship.jpg" class="card-img-top rounded-0">
-            <div class="position-absolute bottom-0 fs-3 ms-2">
-                <p class="mb-1">SpaceX's Starship</p>
+    return (
+        <div className="d-flex flex-column align-content-start justify-content-start">
+            <i className="fa-solid fa-magnifying-glass position-absolute p-2 ms-4 mt-1"></i>
+            <div className="d-flex align-items-center mb-2">
+                <input
+                    type="text"
+                    placeholder="Search Twitter"
+                    className="form-control rounded-5 ps-5 ms-3"
+                />
+                <i className="fa-solid fa-gear mx-5 fa-xl"></i>
             </div>
+            <ul className="nav mb-2 nav-tabs">
+                {renderTabs()}
+            </ul>
+            <div className="position-relative">
+                <img src="../assets/starship.jpg" className="col-sm-12 mt-2" alt="Starship" />
+                <div className="position-absolute bottom-0">
+                    <h2 className="fw-bold ms-3 text-white">SpaceX's Starship</h2>
+                </div>
+            </div>
+            <PostSummaryList />
         </div>
-        ${PostSummaryList()}
-    `;
-};
+    );
+}
 
 export default ExploreComponent;
