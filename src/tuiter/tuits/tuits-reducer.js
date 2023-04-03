15e2a7cc62f44f87ceb99a841c9be121f6ce95f5
Danny Godwin <63467988+dannypgodwin@users.noreply.github.com>
2023-04-03 01:02:22 -0400

    Committing A8.

--- src/tuiter/tuits/tuits-reducer.js
+++ src/tuiter/tuits/tuits-reducer.js
@@ -1,45 +1,3 @@
-// import { createSlice } from "@reduxjs/toolkit";
-// import tuits from './tuits.json';
-//
-// const currentUser = {
-//     userName: "NASA",
-//     handle: "@nasa",
-//     image: "nasa.png",
-// };
-//
-// const templateTuit = {
-//     ...currentUser,
-//     topic: "Space",
-//     time: "2h",
-//     liked: false,
-//     replies: 0,
-//     retuits: 0,
-//     likes: 0,
-// };
-//
-// const tuitsSlice = createSlice({
-//     name: 'tuits',
-//     initialState: tuits,
-//     reducers: {
-//         deleteTuit: (state, action) => {
-//             state.splice(
-//                 state.findIndex(tuit => tuit._id === action.payload),
-//                 1
-//             );
-//         },
-//         createTuit: (state, action) => {
-//             state.unshift({
-//                 ...action.payload,
-//                 ...templateTuit,
-//                 _id: Date.now(),
-//             });
-//         },
-//     },
-// });
-//
-// export const { createTuit, deleteTuit } = tuitsSlice.actions;
-// export default tuitsSlice.reducer;
-
 import {createSlice}
     from "@reduxjs/toolkit";
 import tuits from './tuits.json';
@@ -96,5 +54,5 @@ const tuitsSlice = createSlice({
     reducers: {}
 });
 
-export  const {createTuit, deleteTuit} = tuitsSlice.actions
+export const {createTuit, deleteTuit} = tuitsSlice.actions
 export default tuitsSlice.reducer
